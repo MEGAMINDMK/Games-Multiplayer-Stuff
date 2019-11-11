007f4ffd44cb4ef59a6ebbacafbcad26c7216797
webnet <muhommadraahim1@gmail.com>
2019-11-11 17:42:50 +0500

    Update index.php

--- citizenivmp_map_converter/index.php
+++ citizenivmp_map_converter/index.php
@@ -3,10 +3,10 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<link rel="icon" href="https://citizeniv.net/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="https://citizeniv.net/assets/favicon-f3hd0xob.ico" type="image/x-icon">
 <style>
 body {
-   background-image: url(https://citizeniv.net/images/Light-in-the-Dark/back.jpg);
+   background-image: url(https://citizeniv.net/masterlist/font/back.png);
   background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
